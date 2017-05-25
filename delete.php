bfb3c0dc42134e92e61bc7a3b73a4d5a6185b01d
Prasad Kavinda <pp.kavinda@gmail.com>
2017-05-25 21:53:33 +0530

    added exam.php

--- delete.php
+++ delete.php
@@ -3,10 +3,14 @@ include_once("config.php");
 if(isset($_GET['s_id'])){
     $s_id = $_GET['s_id'];
     $sql = "DELETE FROM students WHERE s_id='$s_id';";
-    
+
 }else if(isset($_GET['l_id'])){
     $l_id = $_GET['l_id'];
     $sql = "DELETE FROM staff WHERE l_id='$l_id'";
+}else if(isset($_GET['date'])){
+    $date = $_GET['date'];
+    $mcode = $_GET['mcode'];
+    $sql = "DELETE FROM exmas WHERE date='$date' AND mcode='$mcode';";
 }
 
 $result = mysqli_query($con, $sql) or die(mysqli_error($con));
--- update_exams.php
+++ update_exams.php
@@ -1,33 +1,31 @@
 <?php
 session_start();
-
-include_once("config.php");
-if(isset($_GET['l_id'])){
-    $l_id = $_GET['l_id'];
-    $sql = "SELECT l_id, fname, lname, dep_id FROM staff WHERE l_id='$l_id';";
-    $result = mysqli_query($con, $sql) or die(mysqli_error($con));
-
-    if(mysqli_num_rows($result)>0){
-        $row=mysqli_fetch_assoc($result);
+if (isset($_GET['date']) && isset($_GET['mcode'])) {
+    include("config.php");
+    $date = $_GET['date'];
+    $mcode = $_GET['mcode'];
+    $sql = "SELECT * FROM exmas WHERE date='$date' AND mcode='$mcode';";
+    $result = mysqli_query($con, $sql);
+    if ($result) {
+        $row = mysqli_fetch_assoc($result);
     }else{
         echo "Error";
     }
-
 }
 if(isset($_POST["submit"])){
-    $l_id = $_POST["l_id"];
-    $fname = $_POST["fname"];
-    $lname = $_POST["lname"];
-    $department = $_POST["department"];
+    $mcode = $_POST["mcode"];
+    $date = $_POST["date"];
 
-    $sql1 = "UPDATE staff SET l_id='$l_id', fname='$fname', lname='$lname', dep_id='$department' WHERE l_id='$l_id';";
+    include_once("config.php");
+    $sql1 = "UPDATE exmas SET date='$date', mcode='$mcode' WHERE date='$date' AND mcode='$mcode';";
     $result1 = mysqli_query($con, $sql1) or die(mysqli_error($con));
 
     if($result1){
         $msg = "* registration successed !";
-        header("Location:admin.php");
+        echo "<script>alert('this is cool.');</script>";
+        header("Location: admin.php");
     }else{
-        $msg = "* Falied! ";
+        $msg = "* Falied!!";
     }
 }
  ?>
@@ -55,29 +53,21 @@ if(isset($_POST["submit"])){
      <div class="container-main">
          <div id="signup" class="tabcontent">
              <ul class="tab tabani">
-                 <li class="tabLi" ><a style="margin-left: 17%;" id="default" class="tablink" >Register Lecture</a></li>
+                 <li class="tabLi" ><a style="margin-left: 17%;" id="default" class="tablink" >Register Student</a></li>
              </ul>
              <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
                  <p style="color: red; margin-left: 5%;">
                      <?php if(isset($msg)) echo $msg; ?>
                  </p>
                  <p>
-                     <label for="sign-id">Id:</label>
-                     <input type="text" name="id" id="sign-id" placeholder="Enter ID" value="<?php echo $row['l_id']?>" required>
+                     <label for="sign-id">Module code:</label>
+                     <input type="text" name="mcode" id="sign-id" placeholder="Enter Module code" value="<?php echo $row['mcode'] ?>" required>
                  </p>
                  <p>
-                     <label for="sign-fname">First Name:</label>
-                     <input type="name" name="fname" id="sign-fname" placeholder="Enter First name" value="<?php echo $row['fname']?>" required>
+                     <label for="sign-fname">Date:</label>
+                     <input type="date" name="date" id="sign-date" placeholder="Enter the Date" value="<?php echo $row['date'] ?>" required>
                  </p>
-                 <p>
-                     <label for="sign-lname">Last Name:</label>
-                     <input type="text" name="lname" id="sign-lname" placeholder="Enter Last name" value="<?php echo $row['lname']?>" required>
-                 </p>
-                 <p>
-                     <label for="sign-department">Department id:</label>
-                     <input type="text" name="department" id="sign-department" placeholder="Department Id" value="<?php echo $row['dep_id']?>" required>
-                 </p>
-                 <input id="submit" type="submit" name="submit"value="Update" onclick="return validatePassword()">
+                 <input id="submit" type="submit" name="submit"value="Register">
              </form>
          </div>
      </div>
